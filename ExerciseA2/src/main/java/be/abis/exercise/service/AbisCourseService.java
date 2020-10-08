c3fc0a9baf9d9a5f87880e60f019f3d90da41f29
krisbroodcoorens <kris.broodcoorens@bnpparibasfortis.com>
2020-10-08 12:36:18 +0200

    Exercise A2

--- ExerciseA2/src/main/java/be/abis/exercise/service/AbisCourseService.java
+++ ExerciseA2/src/main/java/be/abis/exercise/service/AbisCourseService.java
@@ -1,29 +1,34 @@
 package be.abis.exercise.service;
 
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import be.abis.exercise.model.Course;
 import be.abis.exercise.repository.CourseRepository;
 
 @Service
-public class AbisCourseService implements CourseRepository {
+public class AbisCourseService implements CourseService {
 
+	@Autowired
+	private CourseRepository courseRepository;
+	
 	@Override
 	public List<Course> findAllCourses() {
 		// TODO Auto-generated method stub
-		return null;
+		return courseRepository.findAllCourses();
 	}
 
 	@Override
 	public Course findCourse(int id) {
 		// TODO Auto-generated method stub
-		return new Course("7900","Java","Java Reskilling Training",25,550.0);
+		return courseRepository.findCourse(id);
 	}
 
 	@Override
 	public Course findCourse(String shortTitle) {
 		// TODO Auto-generated method stub
-		return new Course ("7900","Java","Java Reskilling Training",25,550.0);
+		return courseRepository.findCourse(shortTitle);
 	}
 
 }
